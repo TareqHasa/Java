056aec4f60dcd68cce7b4812cc32ab49f1052c8b
TareqHasa <mr.tareq.ha@gmail.com>
2021-06-08 23:45:52 +0300

    first java programm

---
+++ javaFun/firstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Tariq Hasan");
+        System.out.println("am 100 years old");
+        System.out.println("My hometown is Nablus");
+    }
+}
