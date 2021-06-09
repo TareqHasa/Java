96753bc9be3c9a5fc34dbfe108c759a9db15239b
TareqHasa <mr.tareq.ha@gmail.com>
2021-06-09 13:12:43 +0300

    Pythagorean

---
+++ javaFun/Pythagorean/Pythagorean.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class Pythagorean {
+    public static double calculateHypotenuse(int legA, int legB) {
+        int num1=legA*legA;
+        int num2=legB*legB;
+        double sum=num1+num2;
+        return Math.sqrt(sum);
+    }
+
+    public static void main(String[] args){
+        int a=4;
+        int b=10;
+        System.out.println(calculateHypotenuse(a,b));
+    }
+}
