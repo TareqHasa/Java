b089454d0f0153c5c41051773f82eb8538bbc3a7
TareqHasa <mr.tareq.ha@gmail.com>
2021-06-17 01:57:34 +0300

    practice session with counter

---
+++ springProjects/counter/src/main/java/com/axsos/counter/CounterController.java
@@ -0,0 +1,32 @@
+package com.axsos.counter;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CounterController {
+	@RequestMapping("/")
+	public String home( HttpSession session) {
+		if (session.getAttribute("counter") == null) {
+			session.setAttribute("counter", 0);
+		} else {
+			Integer count = (Integer) session.getAttribute("counter");
+			count++;
+			session.setAttribute("counter", count);
+
+		}
+
+
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/counter")
+	public String counter(Model model,HttpSession session) {
+		model.addAttribute("count", session.getAttribute("counter"));
+		return "counter.jsp";
+		
+	}
+}
